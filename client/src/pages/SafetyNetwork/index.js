17c173e2f66ac6ef4c751c13e4f6edec58ccc989
Kate Yoak <kate@leanst.com>
2019-11-29 20:39:18 -0800

    SafetyNetwork structure

---
+++ client/src/pages/SafetyNetwork/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Redirect } from 'react-router-dom';
+
+// Components
+import Header from "../../layout/Header";
+import Footer from "../../components/Footer";
+
+import style from "./style.module.scss";
+
+function getSearchParams() {
+  return window.location.search
+    .slice(1)
+    .split('&')
+    .reduce((acc, n) => {
+      const [key, value] = n.split('=');
+      acc[key] = decodeURIComponent(value);
+      return acc;
+    }, {});
+}
+
+
+class SafetyNetwork extends Component {
+  render() {
+
+    const params = getSearchParams();
+
+    /*
+    if (!params.id) {
+      return <Redirect to="/Patron?error=1" />;
+    }
+    */
+
+    return (
+      <div className={style.home}>
+        <Header />
+        <h1>Congratulations! </h1>
+        <div className={style.content}>
+
+          <p>
+          Your safety network is almost ready to get started. Stand by - we'll be sending you information for the next steps soon.
+          </p>
+
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default SafetyNetwork;
